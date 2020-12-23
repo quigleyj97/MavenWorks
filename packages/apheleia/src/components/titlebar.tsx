a89e2ea168700da151af86ae239c1f30e28ac122
Joe Quigley <quigley.joseph@outlook.com>
2020-12-22 16:10:28 -0800

    Add close buttons

--- packages/apheleia/src/components/titlebar.tsx
+++ packages/apheleia/src/components/titlebar.tsx
@@ -5,19 +5,22 @@ import "./_titlebar.scss";
 export interface TitlebarProps {
     /** The text to display in this titlebar */
     text: string;
+    /** Whether this titlebar is active (render prominently) or inactive */
+    isActive: boolean;
     /** Fired whenever the titlebar is tapped or clicked */
     onClick: (this: void) => void;
     /** Fired whenever the titlebar is double-tapped or double-clicked */
     onDblClick: (this: void) => void;
-    /** Whether this titlebar is active (render prominently) or inactive */
-    isActive: boolean;
+    /** Fired whenever the titlebar close button is tapped or clicked */
+    onClose: (this: void) => void;
 }
 
 export const Titlebar: React.FC<TitlebarProps> = ({
     text,
+    isActive=false,
     onClick,
     onDblClick,
-    isActive=false
+    onClose,
 }) => {
     const classList = ["aph-titlebar"];
 
@@ -30,7 +33,13 @@ export const Titlebar: React.FC<TitlebarProps> = ({
     return (<div className={classList.join(" ")}
         onClick={() => onClick.call(void 0)}
         onDoubleClick={() => onDblClick.call(void 0)}>
-            <span className="aph-display-text h3">{text}</span>
+            <span className="title-text aph-display-text h3">{text}</span>
+            <button className="title-close material-icons md-18" onClick={(ev) => {
+                ev.stopPropagation();
+                onClose.call(void 0);
+            }}>
+                close
+            </button>
         </div>
     );
 }
