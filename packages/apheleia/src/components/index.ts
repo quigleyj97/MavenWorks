660cda584672e0aec845dc8e29bd155ad2b2c083
Joe Quigley <quigley.joseph@outlook.com>
2020-12-24 10:58:35 -0500

    Add Part and Stack to index

--- packages/apheleia/src/components/index.ts
+++ packages/apheleia/src/components/index.ts
@@ -1,1 +1,3 @@
 export { Titlebar, TitlebarProps } from "./titlebar/titlebar";
+export { LayoutPart, LayoutPartProps } from "./part/part";
+export { StackPanel, StackPanelProps } from "./stack/stackpanel";
